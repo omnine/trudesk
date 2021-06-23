d4304e0d237657c35f4cf5f9bf9d8085e207824f
omnine <mingfag1@gmail.com>
2021-06-23 17:41:14 +0000

    chore(ewscheck): missed ews definition also propertyset for uniquebody

--- src/mailer/ewsCheck.js
+++ src/mailer/ewsCheck.js
@@ -350,8 +350,11 @@ function openInboxFolder (callback) {
         return callback(null)
       }
       winston.debug('Processing %s Mail', response.TotalCount)
+      var additionalProps = [] // In order to load UniqueBody
+      additionalProps.push(ews.ItemSchema.UniqueBody)
+      var propertySet = new ews.PropertySet(ews.BasePropertySet.FirstClassProperties, additionalProps)
       for (const item of response.items) {
-        item.Load().then(function () {
+        item.Load(propertySet).then(function () {
           var message = {}
           message.from = item.From.Address
           message.subject = item.Subject
@@ -361,7 +364,7 @@ function openInboxFolder (callback) {
           ewsCheck.messages.push(message)
         })
         item.IsRead = true
-        item.Update(ConflictResolutionMode.AutoResolve)
+        item.Update(ews.ConflictResolutionMode.AutoResolve)
       }
       return callback(null)
     },
